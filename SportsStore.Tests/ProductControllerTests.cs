1475231c8fe489d98fc31bc64ca6ecfb73c44d2c
Patrick Huffer <Patrick.Huffer@amtrustfinancial.com>
2018-01-02 16:39:51 -0500

    End of ch08

--- SportsStore.Tests/ProductControllerTests.cs
+++ SportsStore.Tests/ProductControllerTests.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using Moq;
 using SportsStore.Controllers;
 using SportsStore.Data;
@@ -18,15 +17,15 @@ namespace SportsStore.Tests
             var mock = new Mock<IProductRepository>();
             mock.Setup(m => m.Products)
                 .Returns(new[]
-                 {
-                     new Product { ProductID = 1, Name = "P1" },
-                     new Product { ProductID = 2, Name = "P2" },
-                     new Product { ProductID = 3, Name = "P3" },
-                     new Product { ProductID = 4, Name = "P4" },
-                     new Product { ProductID = 5, Name = "P5" }
-                 }.AsQueryable());
+                {
+                    new Product {ProductID = 1, Name = "P1"},
+                    new Product {ProductID = 2, Name = "P2"},
+                    new Product {ProductID = 3, Name = "P3"},
+                    new Product {ProductID = 4, Name = "P4"},
+                    new Product {ProductID = 5, Name = "P5"}
+                }.AsQueryable());
 
-            var controller = new ProductController(mock.Object) { PageSize = 3 };
+            var controller = new ProductController(mock.Object) {PageSize = 3};
 
             // Act
             var result = controller.List(null, 2).ViewData.Model as ProductsListViewModel;
@@ -36,6 +35,34 @@ namespace SportsStore.Tests
             Assert.True(prodArray.Length == 2);
             Assert.Equal("P4", prodArray[0].Name);
             Assert.Equal("P5", prodArray[1].Name);
+        }
+
+        [Fact]
+        public void Can_Send_Pagination_View_Model()
+        {
+            // Arrange
+            var mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products)
+                .Returns(new[]
+                {
+                    new Product {ProductID = 1, Name = "P1"},
+                    new Product {ProductID = 2, Name = "P2"},
+                    new Product {ProductID = 3, Name = "P3"},
+                    new Product {ProductID = 4, Name = "P4"},
+                    new Product {ProductID = 5, Name = "P5"}
+                }.AsQueryable());
+
+            var controller = new ProductController(mock.Object) {PageSize = 3};
+
+            // Act
+            var result = controller.List(null, 2).ViewData.Model as ProductsListViewModel;
+
+            // Assert
+            var pageInfo = result.PagingInfo;
+            Assert.Equal(2, pageInfo.CurrentPage);
+            Assert.Equal(3, pageInfo.ItemsPerPage);
+            Assert.Equal(5, pageInfo.TotalItems);
+            Assert.Equal(2, pageInfo.TotalPages);
         }
     }
 }
--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -13,9 +13,6 @@ namespace SportsStore
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit
-        // https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
@@ -24,8 +21,6 @@ namespace SportsStore
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request
-        // pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
